631d692b97cc70f099af18e98fa951d2f31b2c4d
John Dale <unitejohndalev@gmail.com>
2023-08-15 15:24:05 +0800

    fix error

--- src/hooks/useGameStatus.ts
+++ src/hooks/useGameStatus.ts
@@ -1,5 +1,5 @@
 import { ROWPOINTS } from '@/setup'
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 
 export const useGameStatus = (rowsCleared: number) => {
